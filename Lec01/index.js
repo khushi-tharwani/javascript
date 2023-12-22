2105f2230a596434dd8d8a2728ef9c433215a276
khushi tharwani <tharwanikhushi@gmail.com>
2023-12-22 16:38:53 +0530

    lec1 done

---
+++ Lec01/index.js
@@ -0,0 +1,44 @@
+console.log("hi")
+let x
+
+// typeof-->variable ka datatype batayegaaaa
+
+console.log(typeof x)
+// undefined aayega , kuch hoga hi nhi
+
+let y = null
+console.log(typeof y)
+// object banega but wo null rahega
+let bar=1.5
+console.log(typeof bar)
+
+let a=[1,2,3,"foo"]
+let obj = {
+    name:"John Doe",
+    "email":"test@gmail.com",
+    address:{
+        "buildingname":"XYZ",
+        "streetNme":"" ,
+        "city":"Mumbai",
+        "pincode":"421001"
+    },
+    1:"hieee"
+
+}
+console.log(obj.name)
+console.log(obj[1])
+
+
+
+let num='1.5'
+// let numInt = parseFloat(num)
+// let numInt = Number(num)
+let numInt = +num
+console.log(typeof numInt, numInt)
+
+
+let flag= 0
+if(flag){
+    console.log("hi")
+}
+// falsy value h toh nhi chalegaaaa 
