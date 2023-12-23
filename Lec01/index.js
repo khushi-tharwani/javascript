ce50a13addb646713375d17d15657ae51cb52cbd
khushi tharwani <tharwanikhushi@gmail.com>
2023-12-23 19:02:14 +0530

    lec2

--- Lec01/index.js
+++ Lec01/index.js
@@ -41,7 +41,7 @@ let flag= 0
 if(flag){
     console.log("hi")
 }
-// falsy value h toh nhi chalegaaaa 
+// falsy value h toh nhi chalegaaaa : undefined,0,empty space
 
 
 let str1=1
@@ -58,5 +58,79 @@ console.log(3+ 3)
 
 console.log('3'-'3')
 console.log(3-3)
-console.log('3'-3
-)
+console.log('3'-3)
+
+
+let arr=[1,2,3,4,5]
+// traditional for loop
+for(let i=0; i<arr.length; i++)
+{
+    console.log(arr[i])
+}
+
+// for-of loop
+for(let elem of arr){
+    console.log(elem)
+}
+
+let objc = {
+    name:"Foo",
+    email:"test@gmail.com",
+    number:"12345"
+}
+// for in loop
+for(let key in objc)
+{
+    console.log(key,"-",objc[key])
+}
+// while loop
+let i=0;
+while(i<arr.length)
+{
+    console.log(arr[i]);
+    i++;
+}
+
+
+let n = prompt("enter number:")
+console.log(n)  
+
+// for(let i=2; i<=value; i++)
+// {
+//     if(value%i==0)
+//     {
+//         return 0;
+//     }
+// }
+// return console.log(value);
+
+
+
+
+  let count = 0
+  while (count <= n) {
+    for (let num = 2; count<= n; num++) { 
+      flag=true;
+      for(let i=2; i < num/2; i++)
+      if (num % i === 0) {
+        flag = false;
+        
+      }
+    }
+
+    
+    if (flag) {
+      console.log(num);
+      count++;
+    }
+
+ }
+
+//  declaration function
+
+function addTwoNums(x,y){
+    console.log(x,y)
+    return x+y;
+}
+console.log(addTwoNums(3,4))
+
