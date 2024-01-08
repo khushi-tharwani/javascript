ddd4bc67fcac5ea96ec758248b4e4c6c1f99f951
khushi tharwani <tharwanikhushi@gmail.com>
2024-01-08 16:15:45 +0530

    hw done

---
+++ homework/index.js
@@ -0,0 +1,24 @@
+let numbers = [];
+
+
+document.getElementById("sortButton").addEventListener("click", function() {
+    let inputElement = document.getElementById("inputNumber");
+    let inputValue = Number(inputElement.value);
+    numbers.push(inputValue);
+
+
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+            if (numbers[i] > numbers[j]) {
+            
+                let temp = numbers[i];
+                numbers[i] = numbers[j];
+                numbers[j] = temp;
+            }
+        }
+    }
+
+
+    let resultElement = document.getElementById("sortedResult");
+    resultElement.textContent = `Sorted Numbers: ${numbers.join(', ')}`;
+});
