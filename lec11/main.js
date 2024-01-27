6ef094ff4646728dcf723db2161d9659277ed776
khushi tharwani <tharwanikhushi@gmail.com>
2024-01-27 21:05:08 +0530

    revision of js

--- lec11/main.js
+++ lec11/main.js
@@ -10,3 +10,24 @@ async function hitURL(){
     const value = await response.json()
     console.log(value)
 }
+// important
+// var functionscoped
+// let , const -- blockscoped
+// hoisting in js::: declaration to the top of the current scope(initialization doesnt matter)
+ // (to the top of the current script or the current function).
+//  initialization uper nhi jaayegaaa
+// NULL: object create hua h
+// undefined is always undefined
+// falsy values: not a string, null, nan,undefined
+// for of--array object
+// for in--index k liye , indexes print honge(object keys)
+// callback is asynchronous
+// DOM::Document object model
+// every element in html is represented as an object in java script
+// Events in js: input-> input m jo likha wo likhta jaayega
+// change event :: change hoga click k baad
+// sync->top to bottom,line by line
+// async-> unresponsive rokne k liye, beech m kahi help krega agar data k liye time lagra ho toh
+// callback k liye xml,ajax
+// promises 
+// async await
---
+++ typescript01/index.ts
@@ -0,0 +1,28 @@
+// let x = "test"
+// let str = x.toLowerCase()
+// console.log(str)
+// x();
+// // now it results in compile time error but it actuslly is runtime error in javascript
+
+// function foo(x){
+//     x.flip();
+// }
+// const obj = {
+//     name:"test"
+
+
+// }
+// foo(obj)
+// // if flip would not exist , it would give error on runtime
+// const user= {
+
+// }
+// function flipcoin(){
+//     // meant to be math.randon()
+//     return Math.random()<0.5
+
+// }
+
+// const value = Math.random()< 0.5 ? "a" : "b";
+// if()
+console.log("HIE")
